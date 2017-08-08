b871ace6040d9e2883d653e39e72338cbf23d8b1
Gaven <xu@unnuo.com>
2017-08-08 19:59:48 +0800

    update 自由落体

--- 20170808/main2.js
+++ 20170808/main2.js
@@ -1,21 +1,26 @@
+/**
+ * 
+ */
+// var bridgeWidth = prompt('请输入弦长');
+// var bridgeHeight = prompt('请输入拱高');
+
+// var topAngle = Math.atan((bridgeWidth/2)/bridgeHeight);
+// var centerAngle = Math.PI - (topAngle*2);
+// var R = (bridgeWidth/2) / Math.sin(centerAngle);
+
+// var bridgeLength = centerAngle * R * 2;
+
+// alert('您输入的弦长是：'+bridgeWidth+'米; 拱高是：'+bridgeHeight+'米;最终计算得到的桥面长：'+bridgeLength);
+
+
 var testDom = document.getElementById("Test");
 var testDomTop = 0;
-var testDomLeft = 0;
 var upToDown = true; // true 代表从上往下运动，false代表从下网上
-var leftToRight = true;
 var time = 0;       // 每次运动的当前时间
 var height = 670;   // 下落的具体高度
 var interval = 5; // 多久运动一次 ms
 var g = 9.87*160;
 var maxV = 0;
-
-var randomBgColor = function(dom){
-    var r = Math.round(Math.random()*100+100);
-    var g = Math.round(Math.random()*100+100);
-    var b = Math.round(Math.random()*100+100);
-    console.log("rgb("+r+","+g+","+b+")")
-    dom.style.backgroundColor = "rgb("+r+","+g+","+b+")";
-}
 
 setInterval(function(){
     // console.log(testDomTop);
@@ -32,8 +37,6 @@ setInterval(function(){
     }
 
     if(testDomTop>height){
-        randomBgColor(testDom);
-            
         maxV = (time/1000)*g;
         console.log(maxV);
         time = 0;
@@ -41,26 +44,12 @@ setInterval(function(){
     }
 
     if(testDomTop<0){
-        randomBgColor(testDom);
         // console.log(maxV);
-        
         time = 0;
         upToDown = !upToDown;
     }
 
-    if(leftToRight){
-        testDomLeft++;
-    }else{
-        testDomLeft--;
-    }
-
-    if(testDomLeft<0||testDomLeft>800){
-        randomBgColor(testDom);
-        leftToRight = !leftToRight;
-        // testDom.style.backgroundColor = "rgb(12,23,111)";
-    }
 
     testDom.style.top = testDomTop + "px";
-    testDom.style.left = testDomLeft + "px";
 
 },interval)
