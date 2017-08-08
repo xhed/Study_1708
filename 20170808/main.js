6ec100dfe0ac0026bb73c9256dcdd97992baf300
Gaven <xu@unnuo.com>
2017-08-08 20:18:55 +0800

    update 多球自由落体

--- 20170808/main.js
+++ 20170808/main.js
@@ -1,66 +1,77 @@
-var testDom = document.getElementById("Test");
-var testDomTop = 0;
-var testDomLeft = 0;
-var upToDown = true; // true 代表从上往下运动，false代表从下网上
-var leftToRight = true;
-var time = 0;       // 每次运动的当前时间
-var height = 670;   // 下落的具体高度
-var interval = 5; // 多久运动一次 ms
+var testDom1 = document.getElementById("Test1");
+var testDom2 = document.getElementById("Test2");
+var testDom3 = document.getElementById("Test3");
+var testDom4 = document.getElementById("Test4");
+var testDom5 = document.getElementById("Test5");
+var testDom6 = document.getElementById("Test6");
 var g = 9.87*160;
-var maxV = 0;
 
 var randomBgColor = function(dom){
     var r = Math.round(Math.random()*100+100);
     var g = Math.round(Math.random()*100+100);
     var b = Math.round(Math.random()*100+100);
-    console.log("rgb("+r+","+g+","+b+")")
     dom.style.backgroundColor = "rgb("+r+","+g+","+b+")";
 }
 
-setInterval(function(){
-    // console.log(testDomTop);
-    time+=interval;
+var bindBall = function(dom,g,vx){
 
-    if(upToDown){
-        // testDomTop+=5;
-        testDomTop = 0.5*g*Math.pow(time/1000,2);
-    }else{
-        // testDomTop-=5;
-        // console.log(maxV*time/1000);
-        // console.log(0.5*g*Math.pow(time/1000,2))
-        testDomTop = height - (maxV*time/1000 - 0.5*g*Math.pow(time/1000,2));
-    }
+    var domTop = 0;
+    var domLeft = 0;
+    var upToDown = true; // true 代表从上往下运动，false代表从下网上
+    var leftToRight = true;
+    var time = 0;       // 每次运动的当前时间
+    var height = 665;   // 下落的具体高度
+    var width = 1230;
+    var interval = 5; // 多久运动一次 ms
+    var maxV = 0;
+    randomBgColor(dom);
 
-    if(testDomTop>height){
-        randomBgColor(testDom);
+    setInterval(function(){
+        time+=interval;
+
+        if(upToDown){
+            domTop = 0.5*g*Math.pow(time/1000,2);
+        }else{
+            domTop = height - (maxV*time/1000 - 0.5*g*Math.pow(time/1000,2));
+        }
+
+        if(domTop>height){
+            // randomBgColor(dom);
+                
+            maxV = (time/1000)*g;
+            console.log(maxV);
+            time = 0;
+            upToDown = !upToDown;
+        }
+
+        if(domTop<0){
+            // randomBgColor(dom);
+            // console.log(maxV);
             
-        maxV = (time/1000)*g;
-        console.log(maxV);
-        time = 0;
-        upToDown = !upToDown;
-    }
+            time = 0;
+            upToDown = !upToDown;
+        }
 
-    if(testDomTop<0){
-        randomBgColor(testDom);
-        // console.log(maxV);
-        
-        time = 0;
-        upToDown = !upToDown;
-    }
+        if(leftToRight){
+            domLeft+=vx;
+        }else{
+            domLeft-=vx;
+        }
 
-    if(leftToRight){
-        testDomLeft++;
-    }else{
-        testDomLeft--;
-    }
+        if(domLeft<0||domLeft>width){
+            // randomBgColor(dom);
+            leftToRight = !leftToRight;
+        }
 
-    if(testDomLeft<0||testDomLeft>800){
-        randomBgColor(testDom);
-        leftToRight = !leftToRight;
-        // testDom.style.backgroundColor = "rgb(12,23,111)";
-    }
+        dom.style.top = domTop + "px";
+        dom.style.left = domLeft + "px";
 
-    testDom.style.top = testDomTop + "px";
-    testDom.style.left = testDomLeft + "px";
+    },interval)
+}
 
-},interval)
+bindBall(testDom1,g,1)
+bindBall(testDom2,g,2)
+bindBall(testDom3,g,3)
+bindBall(testDom4,g,4)
+bindBall(testDom5,g,5)
+bindBall(testDom6,g,6)
