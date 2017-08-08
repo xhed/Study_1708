0fe97a8c87b50847c9f40544c0a721e4dde2aa1f
徐佳程 <gaven@Gavens-MacBook-Pro.local>
2017-08-08 14:35:40 +0800

    弦长拱高练习

---
+++ 20170808/main.js
@@ -0,0 +1,10 @@
+var bridgeWidth = prompt('请输入弦长');
+var bridgeHeight = prompt('请输入拱高');
+
+var topAngle = Math.atan((bridgeWidth/2)/bridgeHeight);
+var centerAngle = (Math.PI / 2) - topAngle;
+var R = (bridgeWidth/2) / Math.sin(centerAngle);
+
+var bridgeLength = centerAngle * R * 2;
+
+alert('您输入的弦长是：'+bridgeWidth+'米; 拱高是：'+bridgeHeight+'米;最终计算得到的桥面长：'+bridgeLength);
