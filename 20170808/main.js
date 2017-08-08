e0257ba2ead3fc583e4161bd6473e0901afacfa3
Gaven <xu@unnuo.com>
2017-08-08 17:28:47 +0800

    自由落体运动

--- 20170808/main.js
+++ 20170808/main.js
@@ -16,26 +16,40 @@
 var testDom = document.getElementById("Test");
 var testDomTop = 0;
 var upToDown = true; // true 代表从上往下运动，false代表从下网上
+var time = 0;       // 每次运动的当前时间
+var height = 400;   // 下落的具体高度
+var interval = 10; // 多久运动一次 ms
+var g = 9.87*160;
+var maxV = 0;
 
 setInterval(function(){
-    console.log(testDomTop);
+    // console.log(testDomTop);
+    time+=interval;
 
     if(upToDown){
-        testDomTop+=5;
+        // testDomTop+=5;
+        testDomTop = 0.5*g*Math.pow(time/1000,2);
     }else{
-        testDomTop-=5;
+        // testDomTop-=5;
+        // console.log(maxV*time/1000);
+        // console.log(0.5*g*Math.pow(time/1000,2))
+        testDomTop = height - (maxV*time/1000 - 0.5*g*Math.pow(time/1000,2));
     }
 
-    if(testDomTop<0||testDomTop>200){
+    if(testDomTop>height){
+        maxV = (time/1000)*g;
+        console.log(maxV);
+        time = 0;
         upToDown = !upToDown;
     }
 
+    if(testDomTop<0){
+        // console.log(maxV);
+        time = 0;
+        upToDown = !upToDown;
+    }
+
+
     testDom.style.top = testDomTop + "px";
-},10)
 
-
-// if(){
-
-// }else{
-
-// }
+},interval)
